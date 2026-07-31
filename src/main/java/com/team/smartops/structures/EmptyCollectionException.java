5c84ae1e52191cc408da0d87ac4f902341848b8e
Sark-man <nanasarkodieopoku@gmail.com>
2026-07-31 14:42:18 +0000

    Implemented MyStack push, pop and peek methods without tests

---
+++ src/main/java/com/team/smartops/structures/EmptyCollectionException.java
@@ -0,0 +1,7 @@
+package com.team.smartops.structures;
+
+public class EmptyCollectionException extends RuntimeException {
+        public EmptyCollectionException(String message){
+            super(message);
+        }
+    }
--- src/main/java/com/team/smartops/structures/MyStack.java
+++ src/main/java/com/team/smartops/structures/MyStack.java
@@ -5,6 +5,49 @@ package com.team.smartops.structures;
  * Hand-built implementation -- no java.util.HashMap/Stack/PriorityQueue/etc.
  * Must support: normal case, boundary case, invalid input case (brief Sec 8.iii).
  */
-public class MyStack {
+public class MyStack<T> {
     // TODO: implement required operations (see Section 6 of the brief)
+
+    private Object[] data;
+    private int size;
+
+
+    public MyStack(){
+        data = new Object[4];
+        size = 0;
+    }
+
+    public void push(T value){
+        if (size == data.length) resize();
+        data[size++] = value;
+    }
+
+    @SuppressWarnings("unchecked")
+    public T pop(){
+        if(isEmpty()) throw new EmptyCollectionException("Cannot pop from an empty stack");
+        T top = (T) data[size - 1];
+        data[size - 1] = null;
+        size--;
+        return top;
+    }
+
+    @SuppressWarnings("unchecked")
+    public T peek(){
+        if(isEmpty()) throw new EmptyCollectionException("Cannot peek from an empty stack");
+        return (T) data[size - 1];
+    }
+
+    public boolean isEmpty(){
+        return size == 0;
+    }
+
+    private void resize(){
+        Object[] bigger = new Object[data.length * 2];
+        for (int i = 0; i < data.length; i++) bigger[i] = data[i];
+        data = bigger;
+    }
+
+     public int size(){
+        return size;
+    }
 }
