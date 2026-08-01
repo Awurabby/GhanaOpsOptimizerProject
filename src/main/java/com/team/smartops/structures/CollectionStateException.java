6fab65a19031ef0c1cb03e99e49e5536ba774d02
Sark-man <nanasarkodieopoku@gmail.com>
2026-08-01 23:10:04 +0000

    MyQueue, CircularQueue and Deque completed without tests

---
+++ src/main/java/com/team/smartops/structures/CollectionStateException.java
@@ -0,0 +1,7 @@
+package com.team.smartops.structures;
+
+public class CollectionStateException extends RuntimeException {
+        public CollectionStateException(String message){
+            super(message);
+        }
+    }
